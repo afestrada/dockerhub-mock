442eef30447922054166caaceee306cdbcc1f25e
Andres Estrada <aestrada@stratio.com>
2020-05-12 14:45:28 -0500

    Archivos iniciales

---
+++ src/main/java/com/docker/HolaMundo/controller.java
@@ -0,0 +1,16 @@
+package com.docker.HolaMundo;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class controller extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
